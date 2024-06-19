4675aeddd7ca9aee681848a1424cfa25489f549f
SergiDescayre <lepetitdesca@gmail.com>
2024-06-19 14:53:16 +0200

    console

--- src/components/DayExpenses.jsx
+++ src/components/DayExpenses.jsx
@@ -2,21 +2,24 @@ import { useExpensesContext } from "../context/ExpensesContext";
 import { useTranslation } from "react-i18next";
 
 const DayExpenses = () => {
-  const {t} = useTranslation("global")
-  const { expensesDay,expensePercentage } = useExpensesContext();
+  const { t } = useTranslation("global");
+  const { expensesDay, expensePercentage } = useExpensesContext();
+  console.log("prova");
   return (
     <div className="flex justify-between items-center mt-4 border-t-2 border-gray-400 pt-4  ">
       <div>
-        <div className="text-xs text-gray-500 font-bold">{t("dailyExpenses")}</div>
-        <div className="expenses_day text-[35px] font-semibold">{expensesDay} €</div>
+        <div className="text-xs text-gray-500 font-bold">
+          {t("dailyExpenses")}
+        </div>
+        <div className="expenses_day text-[35px] font-semibold">
+          {expensesDay} €
+        </div>
       </div>
       <div>
         <div className="percentatge text-xs text-right">
-          {
-            expensePercentage >= 0 ? `+${expensePercentage}` : expensePercentage
-          }
-           %
-          </div>
+          {expensePercentage >= 0 ? `+${expensePercentage}` : expensePercentage}
+          %
+        </div>
         <div className="text-xs">{t("comparedYesterday")}</div>
       </div>
     </div>
